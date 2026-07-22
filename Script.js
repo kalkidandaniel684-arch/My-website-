65815bcdc59e0d5e874bf5bbcbec848ae2fb9e2b
kalkidandaniel684-arch <kalkidandaniel684@gmail.com>
2026-07-22 17:37:46 +0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,10 @@
+const loadingSteps = [
+  "Loading HTML...",
+  "Loading CSS...",
+  "Loading JavaScript...",
+  "Checking Internet...",
+  "Searching Birthday Database...",
+  "Target Found ✔",
+  "Authenticating...",
+  "Welcome Efrem 🎉" // Updated target name
+];
